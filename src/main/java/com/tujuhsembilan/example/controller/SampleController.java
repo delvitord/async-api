c816a901f18d5e133b4593e3a452473b59c1e002
delvitord <vitorahim@gmail.com>
2023-09-21 18:57:20 +0700

    100% DoD

--- src/main/java/com/tujuhsembilan/example/controller/SampleController.java
+++ src/main/java/com/tujuhsembilan/example/controller/SampleController.java
@@ -3,8 +3,8 @@ package com.tujuhsembilan.example.controller;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Future;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,42 +22,62 @@ import lombok.RequiredArgsConstructor;
 public class SampleController {
 
     private final PasswordEncoder encoder;
-    private final Map<String, Future<Map<String, String>>> asyncResults = new ConcurrentHashMap<>();
+    private final Map<String, CompletableFuture<Map<String, String>>> asyncResults = new ConcurrentHashMap<>();
 
+    // endpoint buat mulai tugas asinkronnya.
     @GetMapping("/start")
     public ResponseEntity<String> startAsyncProcess() {
         String processId = UUID.randomUUID().toString();
-        asyncResults.put(processId, generateUuidsAsync());
-        return ResponseEntity.ok(processId);
+        CompletableFuture<Map<String, String>> future = new CompletableFuture<>(); // buat CompletableFuture baru.
+        asyncResults.put(processId, future); // masukin CompletableFuture dalam map.
+        String pollingUrl = "/sample/poll/" + processId; // URL untuk buat cek status.
+        startAsyncTask(processId, future); // manggil fungsi async-nya.
+        return ResponseEntity.ok(pollingUrl);
     }
 
-    @GetMapping("/status/{processId}")
-    public ResponseEntity<?> checkStatus(@PathVariable String processId) {
+    // cek status tugas
+    @GetMapping("/poll/{processId}")
+    public ResponseEntity<?> pollStatus(@PathVariable String processId) {
         if (asyncResults.containsKey(processId)) {
-            Future<Map<String, String>> future = asyncResults.get(processId);
-            if (future.isDone()) {
+            CompletableFuture<Map<String, String>> future = asyncResults.get(processId);
+            if (future == null) {
+                return ResponseEntity.ok("Sedang Diproses...");
+            } else if (future.isDone()) {
                 try {
                     Map<String, String> results = future.get();
-                    asyncResults.remove(processId); // Remove completed process
+                    asyncResults.remove(processId);
                     return ResponseEntity.ok(results);
                 } catch (Exception e) {
-                    return ResponseEntity.status(500).body("Error occurred during processing.");
+                    return ResponseEntity.status(500).body("Terjadi Kesalahan Selama Proses.");
                 }
             } else {
-                return ResponseEntity.ok("Processing...");
+                return ResponseEntity.ok("Sedang Diproses...");
             }
         } else {
-            return ResponseEntity.status(404).body("Process not found.");
+            return ResponseEntity.status(404).body("Proses Tidak Ditemukan.");
         }
     }
 
+    // buat fungsi async-nya.
     @Async
-    private Future<Map<String, String>> generateUuidsAsync() {
+    private void startAsyncTask(String processId, CompletableFuture<Map<String, String>> future) {
+        CompletableFuture.runAsync(() -> {
+            Map<String, String> results = generateUuidsAsync();
+            future.complete(results); // CompletableFuture dengan results dari tugas.
+        });
+    }
+
+    // metode buat generate UUID secara asinkron.
+    private Map<String, String> generateUuidsAsync() {
         Map<String, String> results = new HashMap<>();
-        while (results.size() < 1000) {
-            var uuid = UUID.randomUUID().toString();
-            results.put(uuid, encoder.encode(uuid));
+        try {
+            while (results.size() < 10000) {
+                var uuid = UUID.randomUUID().toString();
+                results.put(uuid, encoder.encode(uuid));
+            }
+        } catch (Exception e) {
+
         }
-        return new AsyncResult<>(results);
+        return results;
     }
 }
